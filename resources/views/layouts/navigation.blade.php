1683ae33dda7d69a7a13e1fdf96ac798acb84862
rizky <rizkyaldi0101@gmail.com>
2023-07-26 10:01:23 +0700

    update

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -22,7 +22,7 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('pembeli.list')" :active="request()->routeIs('pembeli')">
+                    <x-nav-link :href="route('pembeli.list')" :active="request()->routeIs('pembeli.list')">
                         {{ __('Pembeli') }}
                     </x-nav-link>
                 </div>
@@ -50,7 +50,7 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('pembeli')">
+                    <x-nav-link :href="route('pembeli.list')" :active="request()->routeIs('pembeli.list')">
                         {{ __('Pembeli') }}
                     </x-nav-link>
                 </div>
